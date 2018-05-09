94fe03278109de6fc9174aa79de38607e99b57be
enricocatanzani <enricocatanzani@95d5289c-d897-42e1-96a8-683723a08b61>
2018-05-09 11:10:59 +0000

    Completed editing sh files, adding comments. Job submission procedure is now comple and working. Remains to fit analysis software

--- trunk/sh_files/submit.sh
+++ trunk/sh_files/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh      
 
 FILELIST="/storage/gpfs_data/dampe/users/ecatanzani/MyRepos/cnaf-jsubmission-svn/trunk/20180501.txt"
-BATCH=50                                                                   #"BATCH" sets the number of how many files evaluate together
+BATCH=50
 
 compute_free() {
     FREE=$MAXPEND
@@ -16,7 +16,7 @@ compute_jobs() {
 compute_maxpend() {
     THRESHOLD_FILE=".THRESHOLD_PENDING"
     SHARPNESS=10
-    echo "1000" > $THRESHOLD_FILE                                          #1000 is the maximum number of pending jobs 
+    echo "1000" > $THRESHOLD_FILE 
     MAXPEND=1000
     MAXPENDL=$MAXPEND
     MAXPENDDEC=$MAXPEND
@@ -63,3 +63,22 @@ echo $nfiles
 if [[ $nfiles -ge 1 ]]; then
     ./submit_one_job.sh $files
 fi
+
+: '
+  COMMENTS ON SH FILE
+
+  This sh file permits to start and manage jobs.
+  Will be used other sub-shell scripts, so the variable management will be described.
+  
+  The maximum number of avaiable jobs is 1000 ("MAXPEND"), while for performance reasons a single job should process more than one file, precisely a number like "BATCH" variable (we fix that value to 50)
+  So we start with an empy string "files", corresponding to a zero value for "nfiles". While we are reading the file list, conteining all the paths of the lifes to process, "files" and "nfiles" are updated.
+  Once the nfiles is bigger than BATCH a job could start, with the big string files as argument. "files" contains the path, separated by a space by construction, of each single file to process.
+  After starting a job, procedure we will explain in the next shell documents, both "files" and "nfiles" return to their original value, respectively an empty string a zero value variable.
+
+  Before updating these variables, and so start a new job if the case, we have to check if the submmitted jobs are more than the free ones.
+  We define a new variable, "MAXPENDL", as "MAXPEND" -20%. So, if submitted jobs are more than free ones, we continue to wait until at least 20% of free "space" is avaiable.
+  This tecnique is used to avoid to "release" the "wait" just for one event; at the countrary if just a new free place shows up, and we immediately start a new job, we will probabily have to wait soon again for a new job submission. 
+  
+  Once whe list has been read, probably some file has noot ben processed; so we winish to process these files. In this case the nuber of processed files will be less that "BATCH" value, infact they have not procecces into the while loop.  
+
+' 
