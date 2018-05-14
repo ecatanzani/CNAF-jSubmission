c20cb4f5f9e9f45f286c1e673878059884fd4764
enricocatanzani <enricocatanzani@95d5289c-d897-42e1-96a8-683723a08b61>
2018-05-14 17:07:59 +0000

    Added comments on sh files and creation list software

--- trunk/Local/sh_files/submit.sh
+++ trunk/Local/sh_files/submit.sh
@@ -63,3 +63,22 @@ echo $nfiles
 if [[ $nfiles -ge 1 ]]; then
     ./submit_one_job.sh $files
 fi
+
+: '
+    COMMENTS ON SH FILE
+
+This sh file permits to start and manage jobs.
+Will be used other sub-shell scripts, so the variable management will be described.
+
+The maximum number of avaiable jobs is 1000 ("MAXPEND"), while for performance reasons a single job should process more than one file, precisely a number like "BATCH" variable (we fix that value to 50)
+So we start with an empy string "files", corresponding to a zero value for "nfiles". While we are reading the file list, conteining all the paths of the lifes to process, "files" and "nfiles" are updated.
+Once the nfiles is bigger than BATCH a job could start, with the big string files as argument. "files" contains the path, separated by a space by construction, of each single file to process.
+After starting a job, procedure we will explain in the next shell documents, both "files" and "nfiles" return to their original value, respectively an empty string a zero value variable.
+
+Before updating these variables, and so start a new job if the case, we have to check if the submmitted jobs are more than the free ones.
+We define a new variable, "MAXPENDL", as "MAXPEND" -20%. So, if submitted jobs are more than free ones, we continue to wait until at least 20% of free "space" is avaiable.
+This tecnique is used to avoid to "release" the "wait" just for one event; at the countrary if just a new free place shows up, and we immediately start a new job, we will probabily have to wait soon again for a new job submission.
+
+Once whe list has been read, probably some file has noot ben processed; so we winish to process these files. In this case the nuber of processed files will be less that "BATCH" value, infact they have not procecces into the while loop.
+
+'
--- trunk/Local/sh_files/template.sh
+++ trunk/Local/sh_files/template.sh
@@ -39,3 +39,41 @@ rm -fv lista.dat
 ls -altrh
 mv -v *.root ${OUTDIR}
 rm -rfv "$SANDBOX"
+
+: '
+    COMMENTS ON SH FILE
+
+This is the template file, used to perform all the operations of the job.
+
+The function run_exec execute the binary file (before compiled apart) to strart data analysis.
+A new random diretcory is created; here all the jobs results are saved, and then moved to the output diretcory. Use use a random diretcory as a sandbox to distinguish are results from other people ones.
+This directory, after files moving, will be deleted.
+
+All environment variables are loaded and "FILES" variables contains all the "BATCH" files to analyse, separated by a space. Now two options are avaiable.
+1 - we can do a loop on all these files (before splited into positional arguments in "submit_one_job.sh" file) and for each one call "fun_exec" function. Here the code to do that:
+
+/////////////////////////////////
+
+for f in $FILES
+do
+run_exec $f
+done
+
+/////////////////////////////////
+
+This is not efficient, so we have not used this tecnique.
+
+2 - we can create a list file and pass it to the function as unique argument. The analysis fotware wil have a function to open this file list and obtain each single path. The code is th one effectively used in the file, here copied:
+
+/////////////////////////////////
+
+FILES="_FILES_"
+echo $FILES >> ./lista.dat
+run_exec lista.dat
+rm -fv lista.dat
+
+/////////////////////////////////
+
+As you can see the list is then deleted.
+
+'
